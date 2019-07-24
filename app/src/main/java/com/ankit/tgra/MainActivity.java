c6f08ca8ab954b69978b56505f3da59bc3b0aa24
ankit <ankit.chaturvedi@manprax.com>
2019-07-24 14:58:10 +0530

    intial commit

---
+++ app/src/main/java/com/ankit/tgra/MainActivity.java
@@ -0,0 +1,100 @@
+package com.ankit.tgra;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    int RC_SIGN_IN = 1;
+    FirebaseAuth mAuth;
+    FirebaseDatabase mdatabase;
+
+    EditText etName, etLocation;
+
+    Button btnNotify;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editText);
+        etLocation = findViewById(R.id.location);
+        btnNotify = findViewById(R.id.button);
+
+
+        //intialize auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //intialize database
+        mdatabase = FirebaseDatabase.getInstance();
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                mdatabase.getReference("Admins").setValue(mAuth.getUid()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "Admin Added", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+            } else {
+                Toast.makeText(MainActivity.this, "operation failed", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+
+    public void signOut(){
+        mAuth.signOut();
+    }
+}
